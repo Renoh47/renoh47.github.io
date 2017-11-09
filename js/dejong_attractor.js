40d0bec163ba0696f8f5cfa26f36ef92babcac9c
Renoh47 <renoh47@gmail.com>
2017-11-09 11:51:13 -0800

    Cleanup of de Jong attractor
    Got rid of a bunch of commented code and unused variables

--- js/dejong_attractor.js
+++ js/dejong_attractor.js
@@ -9,16 +9,15 @@ canvas.onclick = function() {resetDrawing()}
 
 // random attractor params
 var a,b,c,d
-// create points. each aligned to left edge of screen,
-// spread out top to bottom.
 var points = [];
 var numPoints = 1000;
-var scatterScale = 10;
+var renderCount = 0;
 resetDrawing();
 render();
 
 function resetDrawing()
 {
+  renderCount = 0;
   var prevFill = context.fillStyle;
   context.fillStyle = "#000000";
   context.fillRect(0,0,width,height);
@@ -29,55 +28,37 @@ function resetDrawing()
   c = Math.random() * 4 - 2;
   d = Math.random() * 4 - 2;
   points = [];
-  var startX = Math.random()*Math.PI;
-  var startY = Math.random()*Math.PI;
   for (var i = 0; i < numPoints; i++)
   {
     points.push({
-      x: Math.random(),
-      y: Math.random(),
-      vx: 0,
-      vy: 0
+      x: Math.random()*Math.PI,
+      y: Math.random()*Math.PI,
     });
   }
 
-  
+  context.fillStyle = "#FFFFFF";
+  context.strokeStyle = "#FFFFFF";
 }
 
 function render() {
-  
+  var drawX,drawY,vel;
+  var p, newCoords;
   for (var i = 0; i < points.length; i++) {
     // get each point and do what we did before with a single point
-    var p = points[i];
-    var value = getValue(p.x, p.y);
-    context.strokeStyle = "#FAFAFF";
+    p = points[i];
+    // Get new coordinates from the attractor
+    newCoords = getValue(p.x, p.y);
+
     //context.strokeStyle = "rgb(" + Math.floor(p.vx * 50) + "," + Math.floor(p.vy * 50) + "," + Math.floor(Math.abs(p.vx - p.vy)*50) + ")";
-    //console.log(context.strokeStyle);
-    p.vx += Math.cos(value) * 0.3;
-    p.vy += Math.sin(value) * 0.3;
-
-    // move to current position
-    // context.beginPath();
-    // context.moveTo(p.x, p.y);
-
-    // add velocity to position and line to new position
-    p.x = value.x;
-    p.y = value.y;
-    // context.lineTo(p.x, p.y);
-    // context.stroke();
-    context.fillStyle = "#FFFFFF";
-    context.fillRect(p.x * width/4 + width/2, p.y * height/4 + height/2, .5, .5);
-    // apply some friction so point doesn't speed up too much
-    // p.vx *= 0.995;
-    // p.vy *= 0.995;
-
-    // wrap around edges of screen
-    // if (p.x > width) p.x = 0;
-    // if (p.y > height) p.y = 0;
-    // if (p.x < 0) p.x = width;
-    // if (p.y < 0) p.y = height;
+    //Draw single pixel at location
+    if (renderCount > 2) //Avoid drawing first random points
+    {
+      context.fillRect(p.x * width/4 + width/2, p.y * height/4 + height/2, .5, .5);
+    }
+    p.x = newCoords.x;
+    p.y = newCoords.y;
   }
-
+  renderCount++;
   // call this function again in one frame tick
   requestAnimationFrame(render);
 }
@@ -86,14 +67,9 @@ function getValue(x, y) {
   // Peter de Jong attractor
   // http://paulbourke.net/fractals/peterdejong/
 
-  // // scale down x and y
-  // var scale = 0.005;
-  // x = (x - width / 2) * scale;
-  // y = (y - height / 2) * scale;
-
   // attactor gives new x, y for old one.
   var x1 = Math.sin(a * y) - Math.cos(b * x);
   var y1 = Math.sin(c * x) - Math.cos(d * y);
-  // find angle from old to new. that's the value.
+
   return {x: x1, y: y1};
 }
