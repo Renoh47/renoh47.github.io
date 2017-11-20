ee9d44cf721eafec24a8ed93fd22622997b76606
Renoh47 <renoh47@gmail.com>
2017-11-19 17:52:26 -0800

    Prerender moved to web worker, new settings
    * Now uses Web Workers to allow render progress to be displayed
    * New Width, Height, Render progress quicksettings
    * Saves settings in local storage

--- js/dejong_attractor.js
+++ js/dejong_attractor.js
@@ -4,6 +4,16 @@ var canvas = document.getElementById("canvas"),
   height = (canvas.height = window.innerHeight);
 context.font = '12px Arial';
 context.lineWidth = 0.05;
+var renderPercent = 0;
+var preRenderWorker;
+
+if (window.Worker) {
+  console.log("Browser supports web workers, yay!");
+  preRenderWorker = new Worker('js/dejong_worker.js');
+}
+else {
+  alert("Your browser does not support Web Workers, the GUI may freeze while rendering.")
+}
 
 //QuickSettings stuff!
 var params = {};
@@ -12,118 +22,87 @@ params["Iterations: 10^"] = 2;
 params["Draw FPS"] = false;
 params["Draw Lines"] = false;
 params["Show debug info"] = false;
+params["Width"] = width;
+params["Height"] = height;
+
 var settings = QuickSettings.create();
+settings.bindNumber("Width", 0, 10000, Math.floor(width), 1, params);
+settings.bindNumber("Height", 0, 10000, Math.floor(height), 1, params);
 settings.bindRange("Points: 10^", 0, 6, 2, 1, params);
 settings.bindRange("Iterations: 10^", 0, 6, 2, 1, params);
 settings.addButton("Save Image", saveImage);
 settings.addButton("Generate New", resetDrawing);
+settings.addProgressBar("Render Progess", 100, renderPercent, "percent");
 settings.bindBoolean("Draw FPS", false, params);
-settings.bindBoolean("Draw Lines", false, params);
+settings.saveInLocalStorage("dejong_render");
 
-function saveImage()
-{
-  var img = canvas.toDataURL("image/png");
-  var w=window.open('about:blank','image from canvas');
-  w.document.write("<img src='"+img+"' alt='from canvas'/>");
-}
 
-// Generator params
+
+
+// Generator param globals
 var a,b,c,d;
 var points = [];
-var renderCount = 0;
 var imageData;
 var pixelCountArray;
+var minRenderCount = 1;
+var maxRenderCount = Math.pow(10, params["Iterations: 10^"]);
+var doneRendering = false;
+var numPoints = Math.pow(10,params["Points: 10^"]);
 
 var fps, lastTime, delta;
 resetDrawing();
+render();
 
 //Reset the drawing
 function resetDrawing()
 {
+  //apply new settings
+  width = (canvas.width = params["Width"]);
+  height = (canvas.height = params["Height"]);
+  numPoints = Math.pow(10,params["Points: 10^"]);
   shift = Math.floor(Math.random()*360);
   renderCount = 0;
   var prevFill = context.fillStyle;
   context.fillStyle = "#FFFFFF";
   context.fillRect(0,0,width,height);
   context.fillStyle = prevFill;
-  //new parameters, restart points
+  doneRendering = false;
+  // Get new random params, points
   a = Math.random() * 4 - 2;
   b = Math.random() * 4 - 2;
   c = Math.random() * 4 - 2;
   d = Math.random() * 4 - 2;
   points = [];
-  for (var i = 0; i < Math.pow(10,params["Points: 10^"]); i++)
-  {
-    points.push({
-      x: Math.random()*2*Math.PI - Math.PI, // Why Pi? Because magic, that's why.
-      y: Math.random()*2*Math.PI - Math.PI
-    });
-  }
   imageData = context.getImageData(0, 0, width, height);
+  imageData.data.fill(0);
   pixelCountArray = Array(width*height).fill(0);
-  context.fillStyle = "#FFFFFF";
-  context.strokeStyle = "#999999";
-  render();
+
+  // Use a web worker to do the rendering so that we still have responsive interfaces!
+  // Build a message object to send
+  var messageObj = {
+    width: width,
+    height: height,
+    minRenderCount: minRenderCount,
+    maxRenderCount: maxRenderCount,
+    imageData: imageData,
+    pixelCountArray: pixelCountArray,
+    points: points,
+    numPoints: numPoints,
+    deJongParams: [a, b, c, d],
+    shift: shift
+  };
+  // Fire in the hole!
+  preRenderWorker.postMessage(messageObj);
 }
 
 function render() {
-  if (renderCount > Math.pow(10, params["Iterations: 10^"])){
-    context.putImageData(imageData, 0, 0);
-    console.log("Done Rendering!");
-    return;
-  }
-
   if (!lastTime) {
     lastTime = performance.now();
   }
-  var drawX,drawY,vel;
-  var p, newPoint;
-  for (var i = 0; i < points.length; i++) {
-    // get each point and do what we did before with a single point
-    p = points[i];
-    // Get new point from the attractor
-    newPoint = getValue(p.x, p.y);
-    var minRenderCount = 2
-    if (renderCount >= minRenderCount) { //Avoid drawing first random points
-      // Convert from algorithm coords to drawing coords
-      drawX = Math.floor(p.x * width/4 + width/2);
-      drawY = Math.floor(p.y * height/4 + height/2);
-      nextDrawX = newPoint.x * width/4 + width/2;
-      nextDrawY = newPoint.y * height/4 + height/2;
-      deltaX = Math.abs(drawX - nextDrawX);
-      deltaY = Math.abs(drawY - nextDrawY);
-      //Set random color
-      //context.fillStyle = getHSLColorString(Math.floor(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))/(10 * width) + shift);
-      //Draw points 
-      //context.fillRect(drawX, drawY, .5, .5);
-
-      index = (drawX + drawY * imageData.width) * 4;
-      var rgb,hsl;
-      if (renderCount == minRenderCount){
-        hsl = getHSLColor(Math.floor(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))/(10 * width) + shift);
-        rgb = hslToRgb(hsl.h, hsl.s, hsl.l);
-        imageData.data[index + 0] = rgb[0];
-        imageData.data[index + 1] = rgb[1];
-        imageData.data[index + 2] = rgb[2];
-        imageData.data[index + 3] = 0;
-      }
-      //console.log("Drawing point at " + drawX + "," + drawY);
-      imageData.data[index + 3] += 1;
-
-      // Line drawing stuff
-      if (params["Draw Lines"]) {
-        context.strokeStyle = getHSLColorString(p.hsl);
-        context.beginPath();
-        context.moveTo(drawX, drawY);
-        context.lineTo(nextDrawX, nextDrawY);
-        context.stroke();
-      }
-    }
-    // Update the coords
-    p.x = newPoint.x;
-    p.y = newPoint.y;
+  if (doneRendering == true) {
+    context.putImageData(imageData, 0, 0);
   }
-  renderCount++;
+  settings.setValue("Render Progess", renderPercent);
   if (params["Draw FPS"]){
     delta = (performance.now() - lastTime)/1000;
     lastTime = performance.now(); 
@@ -139,26 +118,24 @@ function render() {
   requestAnimationFrame(render);
 }
 
-function getHSLColorString(val) {
-  hue = (val) %360;
-  return "hsl(" + hue + ", 50%, 50%)"
+preRenderWorker.onmessage = function(e){
+  var name = e.data[0];
+  switch(name) {
+    case "renderProgress":
+      renderPercent = e.data[1]; // Update prerendering progress
+      break;
+    case "imageData": // We've received imageData! Yay!
+      imageData = e.data[1];
+      doneRendering = true;      
+      break;
+    default:
+      console.log("Received bork message: " + e);
+  }
 }
 
-function getHSLColor(val){
-  hue = 1.00 * (val % 360) / 360;
-  sat = .5;
-  light = .5;
-  return {h: hue, s: sat, l: light};
-}
-
-
-function getValue(x, y) {
-  // Peter de Jong attractor
-  // http://paulbourke.net/fractals/peterdejong/
-
-  // attactor gives new x, y for old one.
-  var x1 = Math.sin(a * y) - Math.cos(b * x);
-  var y1 = Math.sin(c * x) - Math.cos(d * y);
-
-  return {x: x1, y: y1};
+function saveImage()
+{
+  var img = canvas.toDataURL("image/png");
+  var w=window.open('about:blank','image from canvas');
+  w.document.write("<img src='"+img+"' alt='from canvas'/>");
 }
---
+++ js/dejong_worker.js
@@ -0,0 +1,126 @@
+// Web worker for pre-rendering peter de jong attractor
+
+
+onmessage = function(e){
+  console.log("Message received from main script.");
+  console.log("Starting prerender.");
+  params = e.data;
+  for (var i = 0; i < params.numPoints; i++) {
+    params.points.push({
+      x: Math.random()*4 - 2, 
+      y: Math.random()*4 - 2,
+    });
+  }
+  imageData = prerender(params.width, params.height, params.minRenderCount, params.maxRenderCount, 
+    params.imageData, params.pixelCountArray, params.points, params.deJongParams, params.shift);
+  console.log("Posting message back to main.");
+  postMessage(["imageData", imageData]);
+}
+
+function prerender(width, height, minRenderCount, maxRenderCount, imageData, pixelCountArray, points, deJongParams, shift) {
+  for (renderCount = 0; renderCount <= maxRenderCount; renderCount++) {
+    var donePercentage = Math.floor((renderCount / maxRenderCount) * 100);
+    console.log("Rendering: " + donePercentage);
+    //settings.setValue("Render Progess", renderCount);
+    postMessage(["renderProgress", donePercentage]);
+    var drawX,drawY;
+    var p, newPoint;
+    for (var i = 0; i < points.length; i++) {
+      // get each point and do what we did before with a single point
+      p = points[i];
+      // Get new point from the attractor
+      newPoint = attractorFn(p.x, p.y, deJongParams);
+      if (renderCount >= minRenderCount) { //Avoid drawing first random points
+        // Convert from algorithm coords to drawing coords
+        drawX = Math.floor(p.x * width/4 + width/2);
+        drawY = Math.floor(p.y * height/4 + height/2);
+        nextDrawX = Math.floor(newPoint.x * width/4 + width/2);
+        nextDrawY = Math.floor(newPoint.y * height/4 + height/2);
+        deltaX = Math.abs(drawX - nextDrawX);
+        deltaY = Math.abs(drawY - nextDrawY);
+        index = (drawX + drawY * imageData.width) * 4;
+
+        if (pixelCountArray[index/4] != 1) {
+          var rgb,hsl;
+          hsl = getHSLColor(Math.floor(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))/(10 * width) + shift);
+          rgb = hslToRgb(hsl.h, hsl.s, hsl.l);
+          imageData.data[index + 0] = rgb[0];
+          imageData.data[index + 1] = rgb[1];
+          imageData.data[index + 2] = rgb[2];
+          pixelCountArray[index/4] = 1;
+        }
+        imageData.data[index + 3] += 1;
+
+      }
+      // Update the coords
+      p.x = newPoint.x;
+      p.y = newPoint.y;
+    }
+  }
+  console.log("Done Rendering.");
+  doneRendering = true;
+  return imageData;
+}
+
+// Maybe these should be pulled out and put in the main file and passed. IDK
+
+function getHSLColorString(val) {
+  hue = (val) %360;
+  return "hsl(" + hue + ", 50%, 50%)"
+}
+
+function getHSLColor(val){
+  hue = 1.00 * (val % 360) / 360;
+  sat = .75;
+  light = .5;
+  return {h: hue, s: sat, l: light};
+}
+
+function attractorFn(x, y, vals) {
+  // Peter de Jong attractor
+  // http://paulbourke.net/fractals/peterdejong/
+
+  // attractor gives new x, y for old one.
+  var x1 = Math.sin(vals[0] * y) - Math.cos(vals[1] * x);
+  var y1 = Math.sin(vals[2] * x) - Math.cos(vals[3] * y);
+
+  return {x: x1, y: y1};
+}
+
+//Not sure why, but it couldn't find this function from color-conversion-algorithms.js, even though that is imported first. idk.
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
